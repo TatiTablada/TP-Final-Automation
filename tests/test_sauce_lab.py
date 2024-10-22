d49d9a144ac44446b2d273579e037863139b6344
Tati Tablada <tatitablada14@gmail.com>
2024-10-22 00:39:28 -0300

    pruebas sauce

--- tests/test_sauce_lab.py
+++ tests/test_sauce_lab.py
@@ -19,6 +19,7 @@ class Compras(unittest.TestCase):
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
+        options.add_argument('--remote-debugging-port=9222')
         cls.driver = webdriver.Chrome(options=options)
 
 
