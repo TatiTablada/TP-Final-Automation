f314d10cd818dfc39c9bb391f7179b96baf202b3
Tati Tablada <tatitablada14@gmail.com>
2024-10-22 00:51:40 -0300

    pruebas sauce lab

--- tests/test_sauce_lab.py
+++ tests/test_sauce_lab.py
@@ -16,10 +16,7 @@ class Compras(unittest.TestCase):
     def setUpClass(cls) -> None:
         options = Options()
         options.add_argument('--incognito')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--remote-debugging-port=9222')
+        options.add_argument('--headless')
         cls.driver = webdriver.Chrome(options=options)
 
 
@@ -28,6 +25,7 @@ class Compras(unittest.TestCase):
         cls.driver.quit()
     
     def setUp(self) -> None:
+        #self.driver = webdriver.Chrome()
         load_dotenv()
         base_url = os.getenv('BASE_URL')
         user = os.getenv('USER')
